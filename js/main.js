9618219c13f86a1ee9cbc9152bd42f475ba5105c
ikermentxaka <164804377+ikermentxaka@users.noreply.github.com>
2025-02-10 17:33:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,12 +55,12 @@ document.getElementById("container3D").appendChild(renderer.domElement);
 camera.position.z = objToRender === "dino" ? 25 : 500;
 
 //Add lights to the scene, so we can actually see the 3D model
-const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+const topLight = new THREE.DirectionalLight(0xffffff, 0.5); // (color, intensity)
 topLight.position.set(500, 500, 500) //top-left-ish
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 8 : 3);
 scene.add(ambientLight);
 
 //This adds controls to the camera, so we can rotate / zoom it with the mouse
